017b54bf750aa635dde72860173d30d5be16cc97
engbiu.robocup@gmail.com <Dotan@.(none)>
2010-12-13 19:26:32 +0200

    delete simulation files
    Signed-off-by: engbiu.robocup@gmail.com <Dotan@.(none)>

--- simulation/Robocup 0.2/controllers/khr2/KHR2_Data.d
+++
@@ -1,1 +0,0 @@
-KHR2_Data.o: KHR2_Data.cpp KHR2_Data.h
--- simulation/Robocup 0.2/controllers/khr2/khr2.d
+++
@@ -1,8 +0,0 @@
-khr2.o: khr2.cpp rcb3_simulator.h \
- C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
- C:\Program\ Files\Webots/include/controller/c/webots/types.h \
- C:\Program\ Files\Webots/include/controller/c/webots/servo.h \
- C:\Program\ Files\Webots/include/controller/c/webots/supervisor.h \
- C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
- C:\Program\ Files\Webots/include/controller/c/webots/receiver.h \
- KHR2_Data.h KHR2_Interface.h
